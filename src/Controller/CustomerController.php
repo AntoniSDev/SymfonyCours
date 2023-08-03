19f0cf107101649728f33bf502e5c2dc122eb497
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-08-03 15:21:34 +0200

    afficher données formulaire

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -4,17 +4,28 @@ namespace App\Controller;
 
 use App\Form\CustomerType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Customer;
 
 class CustomerController extends AbstractController
 {
 
   #[Route('/formcustomer', name: 'formcustomer')]
-  public function index()
+  public function index(Request $request)
   {
 
-    $customerform = $this->createForm(CustomerType::class);
+    $customer = new Customer();
+    $customerform = $this->createForm(CustomerType::class, $customer);
+
+    $customerform->handleRequest($request);
+
+    if($customerform->isSubmitted() && $customerform->isValid())
+    {
+      //afficher les données du formulaire
+      dump($request->request->all());
+    }
 
     return $this->render('customer/index.html.twig', [
       'customerform' => $customerform->createView()
